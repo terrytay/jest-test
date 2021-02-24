6b8154378a2e63140cc6369c9120c7e2e2e017c7
Terry Tay <terry@resynctech.com>
2021-02-24 11:00:37 +0800

    Initial setup with jest.config.js

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from "../app/Utils";
+
+describe("Utils test suite", () => {
+	test("First test", () => {
+		const abc = Utils.toUpperCase("abc");
+		console.log("Tests work!");
+	});
+});
