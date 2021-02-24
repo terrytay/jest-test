adc0e89099a7bd52a2889a2b27d61584841ef6c6
Terry Tay <terry@resynctech.com>
2021-02-24 11:23:12 +0800

    expect toEqual

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -5,4 +5,28 @@ describe("Utils test suite", () => {
 		const result = Utils.toUpperCase("abc");
 		expect(result).toBe("ABC");
 	});
+
+	test("Parse simple URL", () => {
+		const url = "http://localhost:8080/login";
+
+		const parsedUrl = Utils.parseUrl(url);
+		expect(parsedUrl.href).toBe(url);
+		expect(parsedUrl.port).toBe("8080");
+		expect(parsedUrl.protocol).toBe("http:");
+		expect(parsedUrl.query).toEqual({});
+	});
+
+	test("Parse URL with query", () => {
+		const url = "http://localhost:8080/login";
+		const query = "?user=user&password=pass";
+		const parsedUrl = Utils.parseUrl(url + query);
+
+		const expectedQuery = {
+			user: "user",
+			password: "pass",
+		};
+
+		expect(parsedUrl.query).toEqual(expectedQuery);
+		expect(expectedQuery).toBe(expectedQuery);
+	});
 });
